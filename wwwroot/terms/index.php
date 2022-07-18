fd5b7eaf2a6fd12a53f6168756d7cb0ee206c0a8
pinnet <danny@dannyarnold.com>
2022-07-18 18:28:05 +0100

    No caching php and html

--- wwwroot/terms/index.php
+++ wwwroot/terms/index.php
@@ -1,4 +1,8 @@
-<?PHP session_start();
+<?PHP 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+session_start();
 $myfile = fopen("index.html", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("index.html"));
 fclose($myfile);
