da4e70c568a91cd9cc60103ee821aa0fcd2e29c6
pinnet <danny@dannyarnold.com>
2022-07-18 12:37:46 +0100

    preprocess index.html

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,31 +1,5 @@
-<!DOCTYPE html>  
-     <head>  
-      <title>Hello World!</title>
-     </head>   
-
-     <body>  
-      <h1>Hello World!</h1>  
-      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>  
-      <?  
-       $database ="website";  
-       $user = "webuser";  
-       $password = "F6bdyDOB8iT23Nfv";  
-       $host = "mysql";  
-
-       $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);  
-       $query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");  
-       $tables = $query->fetchAll(PDO::FETCH_COLUMN);  
-
-        if (empty($tables)) {
-          echo "<p>There are no tables in database \"{$database}\".</p>";
-        } else {
-          echo "<p>Database \"{$database}\" has the following tables:</p>";
-          echo "<ul>";
-            foreach ($tables as $table) {
-              echo "<li>{$table}</li>";
-            }
-          echo "</ul>";
-        }
-        ?>
-    </body>
-</html>
+<?PHP session_start();
+$myfile = fopen("index.html", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("index.html"));
+fclose($myfile);
+?>
