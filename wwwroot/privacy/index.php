80f40a6a3efed8166340968f6e61b24475c12008
pinnet <danny@dannyarnold.com>
2022-07-18 17:57:18 +0100

    BUG: wrong filename

--- wwwroot/privacy/index.php
+++ wwwroot/privacy/index.php
@@ -1,5 +1,5 @@
 <?PHP session_start();
-$myfile = fopen("policy.html", "r") or die("Unable to open file!");
+$myfile = fopen("index.html", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("index.html"));
 fclose($myfile);
 echo "<---".session_id()."--->"; 
