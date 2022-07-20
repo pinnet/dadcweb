4b3cf54c17ee93d09c2bdbc808df15e6e74839ee
pinnet <danny@dannyarnold.com>
2022-07-20 23:41:14 +0100

    add css to webamp

--- wwwroot/webamp/index.php
+++ wwwroot/webamp/index.php
@@ -5,6 +5,7 @@
 <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <META name="viewport" content="width=device-width, initial-scale=1.0">
 <META name="description" content="WebAMP is a web based audio player.">
+<link rel="stylesheet" href="css/style.css">
 <body>
 <div id="app"></div>
 <script src="https://unpkg.com/webamp"></script>
