7ce0ec8e3b234c300f655ebbbb8ee5a630f90164
pinnet <danny@dannyarnold.com>
2022-07-21 23:59:12 +0100

    rplace jsc64 with vice.js

--- wwwroot/jsc64/js/nl/kingsquare/core/cpu/CPUOpcodeInfo.js
+++
@@ -1,9 +0,0 @@
-nl.kingsquare.core.cpu.CPUOpcodeInfo = Class.extend({
-	init: function(cycles /* :uint */ , len/* :uint */, handler/* :Function */, addr/* :Function */, mnemo/* :String */)  {
-		this.cycles = cycles;
-		this.len = len;
-		this.handler = handler;
-		this.addr = addr;
-		this.mnemo = mnemo;
-	}
-});
--- wwwroot/jsc64/js/nl/kingsquare/core/exceptions/BreakpointException.js
+++
@@ -1,8 +0,0 @@
-nl.kingsquare.core.exceptions.BreakpointException = function(message/*:String*/, address/*:uint*/, type/*:uint*/, cyclesConsumed/*:uint*/) {
-	//this.prototype.call(this, message);
-	this.message = message;
-	this.address = address;
-	this.type = type;
-	this.cyclesConsumed = cyclesConsumed;
-}
-//nl.kingsquare.core.exceptions.BreakpointException.prototype = new Error;
