cc933aeb480da4b3c9be3728dce5eb0b6f10570c
pinnet <danny@dannyarnold.com>
2022-07-21 01:50:23 +0100

    Borken it

--- wwwroot/webamp/index.js
+++ wwwroot/webamp/index.js
@@ -1,13 +1,20 @@
 const buttonBindings = [
-    { id: "prev", cb: () => webamp.previousTrack() },
-    { id: "play", cb: () => webamp.play() },
-    { id: "pause", cb: () => webamp.pause() },
-    { id: "stop", cb: () => webamp.stop() },
-    { id: "next", cb: () => webamp.nextTrack() },
-    { id: "close", cb: () => webamp.close() }
-  ];
-  buttonBindings.forEach(({ id, cb }) => {
-    document.getElementById(id).addEventListener("click", cb);
-  });
-  
-  
+  { id: "prev", cb: () => webamp.previousTrack() },
+  { id: "play", cb: () => webamp.play() },
+  { id: "pause", cb: () => webamp.pause() },
+  { id: "stop", cb: () => webamp.stop() },
+  { id: "next", cb: () => webamp.nextTrack() },
+  { id: "close", cb: () => webamp.close() }
+];
+buttonBindings.forEach(({ id, cb }) => {
+  document.getElementById(id).addEventListener("click", cb);
+});
+
+// An example of shoing the currently playing track
+webamp.onTrackDidChange((trackInfo) => {
+  const { artist, title } = trackInfo.metaData;
+  // Another option might be to set `document.title`;
+  document.getElementById(
+    "current-track"
+  ).innerText = `Now playing: ${title} by ${artist}`;
+});
--- wwwroot/webamp/index.php
+++ wwwroot/webamp/index.php
@@ -35,18 +35,6 @@
             ],
         });
 
-        const buttonBindings = [
-            { id: "prev", cb: () => webamp.previousTrack() },
-            { id: "play", cb: () => webamp.play() },
-            { id: "pause", cb: () => webamp.pause() },
-            { id: "stop", cb: () => webamp.stop() },
-            { id: "next", cb: () => webamp.nextTrack() },
-            { id: "close", cb: () => webamp.close() }
-        ];
-        buttonBindings.forEach(({ id, cb }) => {
-        document.getElementById(id).addEventListener("click", cb);
-        });
-
         webamp.renderWhenReady(document.getElementById('app'));
     </script>
 </body>
