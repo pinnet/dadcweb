1bf031094dafa0877cbf42473bed0ca59ecaffa7
pinnet <danny@dannyarnold.com>
2022-07-23 21:42:26 +0100

    Auto-generated build [2022-07-23:21:42:07]

--- wwwroot/terms/index.php
+++ wwwroot/terms/index.php
@@ -4,5 +4,4 @@ require_once "webroot/nocache.php";
 $myfile = fopen("index.html", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("index.html"));
 fclose($myfile);
-echo "<---".session_id()."--->"; 
 ?>
